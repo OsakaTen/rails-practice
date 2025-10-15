14e137a94bc2ecb46243d0f3df36eab1235ad13b
Osaka Ten <Tenkiki107@gmail.com>
2025-10-15 01:48:50 +0000

    Userモデルテストの完成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "バリデーション" do
+  let(:user) do
+    User.new(
+      email: "test@example.com",
+      password: "password",
+      first_name: "太郎",
+      last_name: "テスト"
+    )
+  end
+
+  describe "ユーザー登録のバリデーション（正しく保存できるか）" do
     it "全ての項目が正しく入力されていれば有効であること" do
-      user = User.new(
-        email: "test@example.com",
-        password: "password",
-        first_name: "太郎",
-        last_name: "テスト"
-      )
       expect(user).to be_valid
     end
 
     it "メールアドレスがない場合、無効であること" do
-      user = User.new(
-        email: nil,
-        password: "password",
-        first_name: "太郎",
-        last_name: "テスト"
-      )
+      user.email = nil
       expect(user).not_to be_valid
       expect(user.errors[:email]).to include("can't be blank")
     end
 
     it "パスワードがない場合、無効であること" do
-      user = User.new(
-        email: "test@example.com",
-        password: nil,
-        first_name: "太郎",
-        last_name: "テスト"
-      )
+      user.password = nil
       expect(user).not_to be_valid
       expect(user.errors[:password]).to include("can't be blank")
     end
@@ -50,9 +43,21 @@ RSpec.describe User, type: :model do
       expect(user).not_to be_valid
       expect(user.errors[:email]).to include("has already been taken")
     end
+
+    it "first_name がない場合、無効であること" do
+      user.first_name = nil
+      expect(user).not_to be_valid
+      expect(user.errors[:first_name]).to include("can't be blank")
+    end
+
+    it "last_name がない場合、無効であること" do
+      user.last_name = nil
+      expect(user).not_to be_valid
+      expect(user.errors[:last_name]).to include("can't be blank")
+    end
   end
 
-  describe "インスタンスメソッド" do
+  describe "Userモデルのインスタンスメソッド（動作確認）" do
     it "#full_name が first_name と last_name を結合して返すこと" do
       user = User.new(
         first_name: "太郎",
